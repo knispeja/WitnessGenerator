8d448a0c500326e88cce35a77d76f2aac11a5775
Jacob Knispel <knispel.jacob@gmail.com>
2019-05-13 23:51:43 -0500

    Refactoring, import pSBC

--- js/control.js
+++ js/control.js
@@ -3,9 +3,9 @@ function on_click_start_node(start_node) {
 	var center_y = start_node.getAttributeNS(null, 'cy');
 	var radius = start_node.getAttributeNS(null, 'r');
 
-	append_svg_node(svg, 'circle', { cx: x, cy: y, r: cfg.start_node_radius, fill: cfg.color, onclick: "on_click_start_node(this);" });
+	append_svg_node(svg, 'circle', { cx: center_x, cy: center_y, r: radius, fill: cfg.path_color, onclick: "on_click_start_node(this);" });
 }
 
-function expand_start_node_overlay(start_node_overlay) {[
-	
-]}
+function expand_start_node_overlay(start_node_overlay) {
+
+}
--- js/graphics.js
+++ js/graphics.js
@@ -1,17 +1,3 @@
-class PuzzleDrawConfiguration {
-	constructor(color) {
-		this.color = color;
-		this.background_color = 'grey';
-		this.edge_spacing = 100;
-		this.edge_thickness = 24;
-		this.obstacle_gap_size = this.edge_spacing / 4;
-		this.start_node_radius = this.edge_thickness * 1.2;
-		this.pellet_color = 'black';
-		this.pellet_side_length = this.edge_thickness/2.08;
-		this.colored_square_side_length = 30;
-	}
-}
-
 function init_graphics(draw_config) {
 	cfg = draw_config;
 	svg = append_svg_node(document.body, "svg");
