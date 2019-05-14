ac270fccf57ba8956816921e9a41e6a55ca4ffba
Jacob Knispel <knispel.jacob@gmail.com>
2019-05-14 00:25:46 -0500

    Simple effect when clicking on start node

--- js/control.js
+++ js/control.js
@@ -1,11 +1,28 @@
+var currently_drawing_path = false;
+
 function on_click_start_node(start_node) {
+	if (currently_drawing_path) {
+		return;
+	}
+
 	var center_x = start_node.getAttributeNS(null, 'cx');
 	var center_y = start_node.getAttributeNS(null, 'cy');
-	var radius = start_node.getAttributeNS(null, 'r');
+	var radius = cfg.start_node_radius/10;
 
-	append_svg_node(svg, 'circle', { cx: center_x, cy: center_y, r: radius, fill: cfg.path_color, onclick: "on_click_start_node(this);" });
+	start_node_overlay = append_svg_node(svg, 'circle', { cx: center_x, cy: center_y, r: radius, fill: cfg.path_color });
+	currently_drawing_path = true;
+
+	expand_start_node_timer = setInterval(expand_start_node_overlay, 25);
 }
 
-function expand_start_node_overlay(start_node_overlay) {
+function expand_start_node_overlay() {
+	var prev_radius = parseInt(start_node_overlay.getAttributeNS(null, 'r'));
+	var new_radius = prev_radius + 2;
 
+	if (new_radius >= cfg.start_node_radius) {
+		clearInterval(expand_start_node_timer);
+		new_radius = cfg.start_node_radius;
+	}
+
+	start_node_overlay.setAttributeNS(null, 'r', new_radius);
 }
