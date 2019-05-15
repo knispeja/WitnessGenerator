b9759d78a9fb91d5a66a670987f0aec2290c159c
Jacob Knispel <knispel.jacob@gmail.com>
2019-05-15 00:29:44 -0500

    Create class to manage path, add path removal

--- js/control.js
+++ js/control.js
@@ -1,7 +1,40 @@
-var currently_drawing_path = false;
+class PathDisplay {
+	constructor() {
+		this.currently_drawing_path = false;
+		this.start_node_overlay = null;
+		this.path_objects = [];
+	}
+
+	start_drawing(start_node) {
+		document.body.addEventListener('click', path_display.stop_drawing, true);
+		this.currently_drawing_path = true;
+		this.start_node_overlay = start_node;
+		this.expand_start_node_timer = setInterval(() => this.expand_start_node(this), 5);
+	}
+
+	stop_drawing() {
+		path_display.currently_drawing_path = false;
+		document.body.removeEventListener('click', path_display.stop_drawing, true);
+		svg.removeChild(path_display.start_node_overlay);
+	}
+
+	expand_start_node() {
+		var prev_radius = parseInt(path_display.start_node_overlay.getAttributeNS(null, 'r'));
+		var new_radius = prev_radius + 1;
+	
+		if (new_radius >= cfg.start_node_radius) {
+			clearInterval(path_display.expand_start_node_timer);
+			new_radius = cfg.start_node_radius;
+		}
+	
+		path_display.start_node_overlay.setAttributeNS(null, 'r', new_radius);
+	}
+}
+
+var path_display = new PathDisplay();
 
 function on_click_start_node(start_node) {
-	if (currently_drawing_path) {
+	if (path_display.currently_drawing_path) {
 		return;
 	}
 
@@ -9,20 +42,7 @@ function on_click_start_node(start_node) {
 	var center_y = start_node.getAttributeNS(null, 'cy');
 	var radius = cfg.start_node_radius/10;
 
-	start_node_overlay = append_svg_node(svg, 'circle', { cx: center_x, cy: center_y, r: radius, fill: cfg.path_color });
-	currently_drawing_path = true;
+	var start_node_overlay = append_svg_node(svg, 'circle', { cx: center_x, cy: center_y, r: radius, fill: cfg.path_color });
 
-	expand_start_node_timer = setInterval(expand_start_node_overlay, 25);
-}
-
-function expand_start_node_overlay() {
-	var prev_radius = parseInt(start_node_overlay.getAttributeNS(null, 'r'));
-	var new_radius = prev_radius + 2;
-
-	if (new_radius >= cfg.start_node_radius) {
-		clearInterval(expand_start_node_timer);
-		new_radius = cfg.start_node_radius;
-	}
-
-	start_node_overlay.setAttributeNS(null, 'r', new_radius);
+	path_display.start_drawing(start_node_overlay);
 }
